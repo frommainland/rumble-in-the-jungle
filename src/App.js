8a55af0f7b6baf60f6fce9899b8a0935cc49361c
shuo <caihehuang@gmail.com>
2021-09-14 16:56:06 +0800

    test merge

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-import logo from './logo.svg';
+
 import './App.css';
-<<<<<<< HEAD
 import Comp from './components/AnimalParallax';
-=======
 
 import Parallax from './components/AnimalParallax';
->>>>>>> parent of 5fc55d4 (wrap up for the day)
+
 
 function App() {
     return (
--- src/components/AnimalParallax.js
+++ src/components/AnimalParallax.js
@@ -1,50 +1,14 @@
-
-<<<<<<< HEAD
-import { motion, useMotionValue } from "framer-motion"
-=======
 import { motion, useViewportScroll, useTransform } from "framer-motion";
 import { useEffect, useState } from "react";
->>>>>>> parent of 5fc55d4 (wrap up for the day)
 import bg0 from '../img/animalBg-0.svg'
 import bg1 from '../img/animalBg-1.svg'
 import bg2 from '../img/animalBg-2.svg'
 import bg3 from '../img/animalBg-3.svg'
 import bg4 from '../img/animalBg-4.svg'
 
+const Parallax = () => {
 
-import React, { useEffect, useRef } from 'react';
 
-const Comp = () => {
-    const ref = useRef();
-    const offset = useMotionValue(0)
-
-<<<<<<< HEAD
-    const handleScroll = () => {
-        const posX = ref.current.getBoundingClientRect().left;
-        offset = window.pageXOffset - posX;
-        console.log(offset);
-    };
-
-=======
->>>>>>> parent of 5fc55d4 (wrap up for the day)
-    useEffect(() => {
-        window.addEventListener('scroll', handleScroll);
-
-<<<<<<< HEAD
-        return () => {
-            window.removeEventListener('scroll', handleScroll);
-        };
-    });
-
-    return (
-        <motion.div
-            ref={ref}
-            style={{
-                background: 'red',
-                height: 100,
-                width: '300vw'
-            }}>Contents of your component</motion.div>)
-=======
     return (
         <div>
             <motion.div
@@ -85,7 +49,6 @@ const Comp = () => {
             }}>scrollX: {currentX} {currentXProgress}</p>
         </div>
     )
->>>>>>> parent of 5fc55d4 (wrap up for the day)
 }
 
-export default Comp;
+export default Parallax;
