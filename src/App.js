68c47ae96b92082c43d588a38f985f8c10f0edce
shuo <caihehuang@gmail.com>
2021-09-14 16:52:05 +0800

    test merge

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
-
+import logo from './logo.svg';
 import './App.css';
+<<<<<<< HEAD
 import Comp from './components/AnimalParallax';
+=======
+
+import Parallax from './components/AnimalParallax';
+>>>>>>> parent of 5fc55d4 (wrap up for the day)
 
 function App() {
     return (
--- src/components/AnimalParallax.js
+++ src/components/AnimalParallax.js
@@ -1,5 +1,10 @@
 
+<<<<<<< HEAD
 import { motion, useMotionValue } from "framer-motion"
+=======
+import { motion, useViewportScroll, useTransform } from "framer-motion";
+import { useEffect, useState } from "react";
+>>>>>>> parent of 5fc55d4 (wrap up for the day)
 import bg0 from '../img/animalBg-0.svg'
 import bg1 from '../img/animalBg-1.svg'
 import bg2 from '../img/animalBg-2.svg'
@@ -13,15 +18,19 @@ const Comp = () => {
     const ref = useRef();
     const offset = useMotionValue(0)
 
+<<<<<<< HEAD
     const handleScroll = () => {
         const posX = ref.current.getBoundingClientRect().left;
         offset = window.pageXOffset - posX;
         console.log(offset);
     };
 
+=======
+>>>>>>> parent of 5fc55d4 (wrap up for the day)
     useEffect(() => {
         window.addEventListener('scroll', handleScroll);
 
+<<<<<<< HEAD
         return () => {
             window.removeEventListener('scroll', handleScroll);
         };
@@ -35,6 +44,48 @@ const Comp = () => {
                 height: 100,
                 width: '300vw'
             }}>Contents of your component</motion.div>)
+=======
+    return (
+        <div>
+            <motion.div
+                style={{
+                    width: 1888,
+                    height: 375,
+                    backgroundImage: `url(${bg2})`,
+                    backgroundSize: '100% 100%',
+                    position: 'absolute',
+                    left: 0,
+                    top: 0,
+                    display: 'inline-block'
+                }} />
+            <motion.div
+                style={{
+                    width: 1888,
+                    height: 375,
+                    backgroundImage: `url(${bg1})`,
+                    backgroundSize: '100% 100%',
+                    position: 'absolute',
+                    left: 0,
+                    top: 0,
+                    display: 'inline-block'
+                }} />
+            <motion.div
+                style={{
+                    width: 1888,
+                    height: 375,
+                    backgroundImage: `url(${bg0})`,
+                    backgroundSize: '100% 100%',
+                    position: 'absolute',
+                    left: 0,
+                    top: 0,
+                    display: 'inline-block'
+                }} />
+            <p style={{
+                position: 'fixed'
+            }}>scrollX: {currentX} {currentXProgress}</p>
+        </div>
+    )
+>>>>>>> parent of 5fc55d4 (wrap up for the day)
 }
 
 export default Comp;
