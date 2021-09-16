5474e3591ea8cf9e1577345e05467238ef1209f3
shuo <caihehuang@gmail.com>
2021-09-16 16:40:56 +0800

    Update Prologue.js

--- src/components/Prologue.js
+++ src/components/Prologue.js
@@ -10,12 +10,30 @@ export default function Prologue() {
 
     const { scrollXProgress, scrollX } = useViewportScroll();
 
+    const [currentX, setCurrentX] = useState(0)
+
+    // bg3 anim
     const bg3Scale = useTransform(scrollX, [0, 700], [1, 5])
     const rotate = useTransform(scrollX, [0, 700], [1, 40])
     const filter = useTransform(scrollX, [0, 700], [0, 20])
     const bg3Filter = useMotionTemplate`blur(${filter}px)`
 
+    // bg2 anim
+    const bg2Scale = useTransform(scrollX, [0, 900], [1, 3])
+    const bg2Opacity = useTransform(scrollX, [0, 900], [.6, 1])
+    const bg2Rotate = useTransform(scrollX, [0, 900], [1, 60])
 
+
+    // text anim
+    const textScale = useTransform(scrollX, [0, 400], [1, 25])
+
+
+    useEffect(() => {
+        const unsubscribeX = scrollX.onChange((v) => setCurrentX(v.toFixed()))
+        return () => {
+            unsubscribeX()
+        }
+    })
 
     return (
         <div style={{
@@ -34,18 +52,29 @@ export default function Prologue() {
                     top: 0,
                     opacity: .4
                 }} />
-            <motion.div
-                className='bg2'
+            <div
+                className='bg2Wrap'
                 style={{
                     width: '100vw',
-                    height: '100vh',
-                    backgroundImage: `url(${bg2})`,
-                    backgroundSize: 'cover',
-                    position: 'absolute',
-                    left: 0,
-                    top: 0,
-                    opacity: .6
-                }} />
+                    height: '100vh'
+                }}>
+                <motion.div
+                    className='bg2'
+                    style={{
+                        width: '120vw',
+                        height: '120vh',
+                        backgroundImage: `url(${bg2})`,
+                        backgroundSize: 'cover',
+                        position: 'absolute',
+                        left: '50%',
+                        top: '50%',
+                        marginLeft: '-60vw',
+                        marginTop: '-60vh',
+                        opacity: bg2Opacity,
+                        scale: bg2Scale,
+                        rotate: bg2Rotate
+                    }} />
+            </div>
             <motion.div
                 className='bg3'
                 style={{
@@ -65,13 +94,17 @@ export default function Prologue() {
                 color: 'white',
                 fontFamily: 'CyrulikRounded',
                 position: 'absolute',
+                left: 0,
+                top: 0,
                 width: '100%',
                 height: '100%',
                 display: "flex",
                 flexWrap: 'nowrap',
                 flexDirection: 'column',
                 justifyContent: 'center',
-                alignItems: 'center'
+                alignItems: 'center',
+                scale: textScale,
+                originY: 0.25
             }}>
                 <h1 style={{
                     fontSize: 80,
@@ -82,6 +115,11 @@ export default function Prologue() {
                     margin: 0
                 }}>In The Jungle</h3>
             </motion.div>
+            <h1
+                style={{
+                    position: "fixed",
+                    color: 'white'
+                }}>{currentX}</h1>
         </div>
     )
 }
